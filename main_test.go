6c684cb461a2f21e8385af1c4df95beb557e648c
Tejzpr <tejzpr@gmail.com>
2021-02-27 19:13:05 -0800

    Update README.md

---
+++ main_test.go
@@ -0,0 +1,36 @@
+package orderedconcurrently
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+// The work that needs to be performed
+func work(val interface{}) interface{} {
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+	return val
+}
+
+func Test(t *testing.T) {
+	max := 10
+	inputChan := make(chan *OrderedInput)
+	wg := &sync.WaitGroup{}
+	go func() {
+		outChan := Process(inputChan, work, 10)
+		for out := range outChan {
+			fmt.Println(out.Value)
+			wg.Done()
+		}
+	}()
+
+	// Create work and the associated order
+	for work, order := 0, 0; work < max; work, order = work+1, order+1 {
+		wg.Add(1)
+		input := &OrderedInput{work, order}
+		inputChan <- input
+	}
+	wg.Wait()
+}
